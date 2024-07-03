1d2c74f540c238c8247ae8b17fac956a8f604d15
manmaru0824 <165455265+manmaru0824@users.noreply.github.com>
2024-07-03 20:30:13 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,27 @@
+!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+
+       <?php
+       $product = array(
+       "name" => "onion",
+       "price" => 200,
+       "weight" => 160
+       );
+       ?>
+
+       <?php
+       print_r($product);
+       ?>
+
+    </p>
+</body>
+     
+</html>
