f94a12b03da0784b8a416416acd4ba4c1be755fc
manmaru0824 <165455265+manmaru0824@users.noreply.github.com>
2024-07-06 13:21:51 +0900

    kadai_016を修正2

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -1,38 +1,77 @@
-<?php
-class Food {
-    private $name;
-    private $price;
-    public function show_price(int $price) {
-        $this->price = $price;
-    }
-    public function __construct(string $name, int $price) {
-        $this->name = $name;
-        $this->price =$price;
-    }   
-}
-$food = new Food('potato', 250);
-print_r($food);
-echo '<br>';
+<!DOCTYPE html>
+<html lang="ja">
 
-class Animal {
-    private $name;
-    private $height;
-    private $weight;
-    public function show_height(int $height) {
-        $this->height = $height;
-    }
-    public function __construct(string $name, int $height, int $weight) {
-            $this->name = $name; 
-            $this->height = $height;
-            $this->weight = $weight;
-    }     
-}
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_016</title>
+</head>
 
-$animal= new Animal('dog', 60, 5000);
-print_r($animal);
+<body>
 
-$food->show_price(250);
+    <p>
+        <?php
+        // Foodクラスを定義する
+        class Food {
+            // プロパティを定義する  
+            private $name;
+            private $price;
 
-$animal->show_height(60);
+            // メソッドを定義する
+            public function __construct(string $name, int $price)
+            {
+                $this->name = $name;
+                $this->price = $price;
+            }
+            public function show_price()
+            {
+                echo $this->price;
+            }
+        }
 
-?>
+        // インスタンス化する
+        $food = new Food('potato', 250);
+
+        // インスタンス$foodの各プロパティの値を出力する
+        print_r($food);
+        echo '<br>';
+
+        // Animalクラスを定義する
+        class Animal {
+            // プロパティを定義する  
+            private $name;
+            private $height;
+            private $weight;
+
+            // メソッドを定義する
+            public function __construct(string $name, int $height, int $weight)
+            {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+            public function show_height()
+            {
+                echo $this->get_height();
+            }
+            public function get_height()
+            {
+                return $this->height;
+            }
+        }
+
+        // インスタンス化する
+        $animal = new Animal('dog', 60, 5000);
+
+        // インスタンス$foodの各プロパティの値を出力する
+        print_r($animal);
+        echo '<br>';
+
+        //プロパティの値を出力する
+        $food->show_price();
+        echo '<br>';
+        $animal->show_height();
+
+        ?>
+    </p>
+
+</html>
